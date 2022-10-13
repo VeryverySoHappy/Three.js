ec9384678c0cb32efee0359a59b6f2bdbeb6cd4b
VeryverySoHappy <leejh@3ibank.com>
2022-10-13 09:57:31 +0900

    module

---
+++ 01/01/start/02_module/hello.js
@@ -0,0 +1,8 @@
+export function hello1() {
+  console.log('Hello 1!');
+}
+
+// 만약에 함수를 여러개 연결해주고 싶으면
+export function hello2() {
+  console.log('Hello 2!');
+}
---
+++ 01/01/start/02_module/main.js
@@ -0,0 +1,13 @@
+/*
+import { hello1 } from './hello.js' ;
+// 모듈일 경우 현재 페이지는 ./를 붙여줘야됌
+
+import { hello2 } from './hello.js'
+이렇게 연결해주거나
+*/
+
+import {hello1, hello2 } from './hello.js'
+// 이렇게 해줌
+
+hello1();
+hello2();
